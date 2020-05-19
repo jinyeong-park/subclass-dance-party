6f0dd84b18dd894c8eb519094384865fb17c4bc4
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-05-18 18:24:31 -0700

    blinking is working

--- src/dancer.js
+++ src/dancer.js
@@ -7,7 +7,9 @@ var makeDancer = function(top, left, timeBetweenSteps) {
 
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
-  this.step.call(this);
+  //already pointed to this, no need to use .call
+
+  this.step();
   this.setPosition(top, left);
 };
 
@@ -16,11 +18,12 @@ makeDancer.prototype.step = function() {
 // the basic dancer doesn't do anything interesting at all on each step,
 // it just schedules the next step
   setTimeout(this.step.bind(this), this.timeBetweenSteps);
+  //not working
   // setTimeout(function() {
-  //     this.step;
+  //   this.step.call(this);
   // }, this.timeBetweenSteps);
-
 };
+
 // dancer.step();
 makeDancer.prototype.setPosition = function(top, left) {
   // Use css top and left properties to position our <span> tag
